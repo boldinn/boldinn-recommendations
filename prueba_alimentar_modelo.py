b141519d09e4ceee9753d68465391832273634ec
oscar blum <oscar.blum@gmail.com>
2023-10-31 14:42:31 -0500

    fix identation

--- prueba_alimentar_modelo.py
+++ prueba_alimentar_modelo.py
@@ -1,4 +1,4 @@
-import json 
+import json
 from alimentar_modelo import actualizar_datos_entrenamiento
 import pandas as pd
 import random
@@ -6,21 +6,25 @@ import requests
 
 
 # Cargar información de ejercicios
-ejercicios_df = pd.read_csv('ejercicios.csv')
+ejercicios_df = pd.read_csv("ejercicios.csv")
 
 # Códigos de los criterios (no están del 1 al 20)
-codigos_criterios=ejercicios_df['criterion_code'].unique()
+codigos_criterios = ejercicios_df["criterion_code"].unique()
 
 utilidades = [0.2, 0.3, 0.5, 0.4]
-puntajes_ejercicios = {'E10': 0.2, 'E20': 0.4, 'E30': 0.4, 'E40':0.6}
+puntajes_ejercicios = {"E10": 0.2, "E20": 0.4, "E30": 0.4, "E40": 0.6}
 
 # 20 puntajes
-P={int(c): 100*random.random() for c in codigos_criterios}
+P = {int(c): 100 * random.random() for c in codigos_criterios}
 
-content={'utilidades':utilidades, 'puntajes_ejercicios':puntajes_ejercicios , 'puntajes':P}
-json=json.dumps(content)
+content = {
+    "utilidades": utilidades,
+    "puntajes_ejercicios": puntajes_ejercicios,
+    "puntajes": P,
+}
+json = json.dumps(content)
 
-#print(json)
+# print(json)
 # Realiza una solicitud POST a la ruta /recomendar_ejercicios
 url = "http://localhost:5000/actualizar_datos"
 response = requests.post(url, json=json)
--- prueba_recomendaciones.py
+++ prueba_recomendaciones.py
@@ -1,30 +1,29 @@
-
 import pandas as pd
 from recomendaciones import recomendar_ejercicios
-import json,random
+import json, random
 import requests
 
 # Cargar información de ejercicios
-ejercicios_df = pd.read_csv('ejercicios.csv')
+ejercicios_df = pd.read_csv("ejercicios.csv")
 
 # Códigos de los criterios (no están del 1 al 20)
-codigos_criterios=ejercicios_df['criterion_code'].unique()
+codigos_criterios = ejercicios_df["criterion_code"].unique()
 
 # Generar un caso
 
 # 20 puntajes
-P={int(c): 100*random.random() for c in codigos_criterios}
+P = {int(c): 100 * random.random() for c in codigos_criterios}
 
 # ejercicios resueltos
-ej=['E1','E3','E5','E7']
+ej = ["E1", "E3", "E5", "E7"]
 
 # Prueba
-print('Puntajes:',P)
-print('Ejercicios resueltos:', ej)
+print("Puntajes:", P)
+print("Ejercicios resueltos:", ej)
 
-content={'puntajes':P, 'ejercicios_resueltos':ej}
-json=json.dumps(content)
-#print(json)
+content = {"puntajes": P, "ejercicios_resueltos": ej}
+json = json.dumps(content)
+# print(json)
 # Realiza una solicitud POST a la ruta /recomendar_ejercicios
 url = "http://localhost:5000/recomendar_ejercicios"
 response = requests.post(url, json=json)
@@ -38,4 +37,3 @@ if response.status_code == 200:
     print(data)
 else:
     print("Error en la solicitud a /recomendar_ejercicios:", response.status_code)
-
